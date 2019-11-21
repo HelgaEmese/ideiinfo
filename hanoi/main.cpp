85256f4c47495aafb9464dcd0073abb060926bb8
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-21 10:45:27 +0200

    hanoi rekurzivitasa

---
+++ hanoi/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int number ,char from, char to ,char holder){
+if (number==1){
+    cout << from << "->" << to << " " << number << ". korong " << endl;
+    return;
+}
+hanoi(number-1,from,holder,to);
+cout<< from << "->" << to << " " << number << ". korong " << endl;
+ hanoi(number-1,holder,to,from);
+
+}
+
+int main()
+{
+hanoi (2, 'A','C', 'B');
+    return 0;
+}
