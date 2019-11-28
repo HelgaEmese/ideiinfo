ff4f73b2cd61e6b644b4bd7f134cc97115e1f1fe
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-28 07:42:58 +0200

    Add files via upload

---
+++ input/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{ int n,i,v[1000],min,max,a;
+    fstream in("input.txt");
+    in>>n;
+     in>>i;
+    int min=i, max=i;
+    while (in>>i){
+        if (min>i){
+            min=i;
+        }
+        if(max<i)
+            max=i;
+    }
+  int temp_min =min;
+  int hatvany =1;
+  while
+
+    cout << << endl;
+    return 0;
+}
