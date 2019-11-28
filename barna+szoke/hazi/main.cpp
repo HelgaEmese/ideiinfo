8dda04892ae7b33c8e7f114eb22bdc992b0ce00f
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-28 07:29:43 +0200

    Add files via upload

---
+++ barna+szoke/hazi/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct diak {
+string diak;
+string nev;
+string hajszin;
+float matekjegy ; }
+xib[32];
+
+int main()
+{
+    int n;
+    cout << " Hany diak van a 11 B-e? " << endl;
+    for (int i=0;i<n;i++)
+    {
+        cout << "diak neve:" << endl;
+    }
+        cin >> xib[i].nev;
+        cout << xib[i].neve << "hajszin";
+        cin << xib[i].hajszin;
+        cout << xib[i].nev << "matek jegy ";
+        cin >> xib[i].matek jegy;
+        int szoke =0 , barna=0;
+        float Sszoke , Sbarna;
+         Sszoke=0; Sbarna=0;
+         for (int i=0;i<n;i++){
+            if (xib[i])
+         }
+
+    return 0;
+}
