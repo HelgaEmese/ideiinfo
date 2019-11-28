5f85e160909752ef2e557f69c6631d08048991a8
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-28 07:45:30 +0200

    Add files via upload

---
+++ lab11es2/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+//a két számot deklaráljuk lokálisan és CÍM szerint adjuk át a függvényeknek, segédváltozót használva ahol szükséges
+
+using namespace std;
+int a, b;
+int lnko (int &a, int &b)
+{
+int a1=a;
+int b1=b;
+    while (a1!=b1)
+    {
+        if(a1>b1)
+        {
+            a1-=b1;
+        }
+        else
+        {b1-=a1;
+        }
+    }
+    return a1;
+}
+int lkkt (int a, int b)
+{
+    return a*b/lnko(a,b);
+}
+
+int main()
+{
+    a=2;
+    b=3;
+
+    cout <<lnko(a,b)<< endl;
+            b1-=a1;
+    cout <<lkkt (a,b) << endl;
+    return 0;
+}
