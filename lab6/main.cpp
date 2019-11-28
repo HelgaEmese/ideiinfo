4233b7b04584105103a0db5880df3bbc136b73aa
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-28 07:44:28 +0200

    Add files via upload

---
+++ lab6/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  int N,M,k,szam;
+  fstream input("input.txt");
+  fstream output("output.txt")
+  for (int i=0;i<N;i++){
+    for (int j=0;j<N;j++){
+            input matrix[i][j];
+            if ()
+    }
+  }
+
+
+
+
+
+
+
+    cout <<  << endl;
+    return 0;
+}
