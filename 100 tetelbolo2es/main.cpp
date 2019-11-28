6a35593fb770baeb1c86d170d442ec4293d82d9d
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-11-28 07:36:42 +0200

    100 tetelbolo2es

---
+++ 100 tetelbolo2es/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ string s;
+  cout << "kerem a beolvasando stringet";
+  getline(cin,s1);
+  size_hely;
+   size_hely1=1;
+
+
+
+
+
+
+    cout <<  << endl;
+    return 0;
+}
