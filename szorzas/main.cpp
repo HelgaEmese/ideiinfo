571c53862fc1d0be53fc8c5fbbcbd882ebf2bbe6
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-12-03 07:15:25 +0200

    Add files via upload

---
+++ szorzas/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Olvassunk be 2 NxN es mátrixot billentyűzetről,
+//majd írjuk ki az A+B illetve az AxB mátrixokat.
+
+
+int main()
+{
+    int A[100][100],B[100][100],SZ[100][100],S[100][100];
+    int i,j,l,k,n
+   cout<<"n=";
+   cin>>n;
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<< "A[ "<<]
+
+
+
+        }
+    }
+
+
+
+    cout <<  << endl;
+    return 0;
+}
