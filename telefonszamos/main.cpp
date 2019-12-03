b0b72eca48e77da97326571e8783839d58659e56
HelgaEmese <47551969+HelgaEmese@users.noreply.github.com>
2019-12-03 07:15:59 +0200

    Add files via upload

---
+++ telefonszamos/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    int n;
+    string v[100][100];
+    input >> n;
+
+
+
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
